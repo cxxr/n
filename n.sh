292c3e3b568be9fababf5c49e0480f1b90214faf
Sean Cassidy <sean.a.cassidy@gmail.com>
2013-12-15 15:00:28 -0800

    Initial commit

---
+++ n.sh
@@ -0,0 +1,41 @@
+#
+# n.sh
+#
+# iterates over files N lines at a time
+#
+# Sean Cassidy <sean.a.cassidy@gmail.com>
+#
+#
+
+STATE_FILE="$HOME/.nstate"
+
+n() {
+    INCREMENT=10
+    if [ -r "$1" ]; then
+        LINE=0
+        FILE=$1
+    else
+        LINE=`cut -f 1 $STATE_FILE`
+        FILE=`cut -f 2 $STATE_FILE`
+
+        if [ -n $1 ]; then
+            # Test for number
+            case $1 in
+                ''|*[!0-9]*) 
+                    echo "Invalid argument to n"
+                    exit 2
+                ;;
+                *)
+                    INCREMENT=$1
+                ;;
+            esac
+        fi
+    fi
+
+
+    HEAD_LEN=$(($LINE + $INCREMENT))
+
+    head -n $HEAD_LEN $FILE | tail -n $INCREMENT
+
+    echo -e "$HEAD_LEN\t$FILE" > $STATE_FILE
+}
